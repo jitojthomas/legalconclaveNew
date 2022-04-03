e1dd28bfe50264d0bbf04d968dd42c0ede5b020d
jitojthomas <102741701+jitojthomas@users.noreply.github.com>
2022-04-03 11:21:28 +0530

    First  Upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,19 @@
+
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDiLyufvE-fTz9n6AHvGvq19_r1qJoDi6Q",
+    authDomain: "legal-conclave-v2.firebaseapp.com",
+    projectId: "legal-conclave-v2",
+    storageBucket: "legal-conclave-v2.appspot.com",
+    messagingSenderId: "130161920405",
+    appId: "1:130161920405:web:c39bee009162140d124e9c",
+    measurementId: "G-0LT4KJZ27E"
+  }
+};
+
+ 
+
+  
+  
+  
